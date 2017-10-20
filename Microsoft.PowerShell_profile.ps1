3dbf4880a6e20ca67f3fc78efb76a90356f9eb9d
Tomáš Kuba <tomas.kuba@simplity.eu>
2017-10-20 14:35:03 +0200

    remove chocolatay

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,8 @@
-Import-Module -Name posh-git
-
 if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadline
     Set-PSReadlineKeyHandler -Key Tab -Function Complete
 }
-
 
 # Persistent History
 $HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
@@ -14,7 +11,6 @@ if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 # Persistent History
-
 
 Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
                          -BriefDescription SmartInsertQuote `
@@ -40,201 +36,23 @@ Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
 
 Start-SshAgent
 
-#shorcut to CopyLastCommandToClipboard
+# shorcut to CopyLastCommandToClipboard
 function CopyLastCommandToClipboard {(Get-History)[-1].CommandLine | clip}
 Set-Alias cc -value CopyLastCommandToClipboard
 
-
-#shortcut to parent folder
+# shortcut to parent folder
 Set-Alias .. -value cd..
 
-write-host ":: Hello tomio, your favourite commands are: cc,                              ::"
+# motd
+write-host ":: Hello tomio, your favourite commands are: cc, ..                           ::"
 write-host ":: feel free to add more commands anytime                                     ::"
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
 
-function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-function prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-
-    Write-Host
-
-    # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-
-    if (Test-Administrator) {  # Use different username if elevated
-        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
-    }
-
-    Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor DarkYellow
-    Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
-
-    if ($s -ne $null) {  # color for PSSessions
-        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
-        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
-        Write-Host ") " -NoNewline -ForegroundColor DarkGray
-    }
-
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
-    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
-    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
-
-    $global:LASTEXITCODE = $realLASTEXITCODE
-
-    Write-VcsStatus
-
-    Write-Host ""
-
-    return "> "
-}
-
-function Get-ChildItem-Color {
-    if ($Args[0] -eq $true) {
-        $ifwide = $true
-
-        if ($Args.Length -gt 1) {
-            $Args = $Args[1..($Args.length - 1)]
-        } else {
-            $Args = @()
-        }
-    } else {
-        $ifwide = $false
-    }
-
-    if (($Args[0] -eq "-a") -or ($Args[0] -eq "--all")) {
-        $Args[0] = "-Force"
-    }
-
-    $width =  $host.UI.RawUI.WindowSize.Width
-    
-    $items = Invoke-Expression "Get-ChildItem `"$Args`"";
-    $lnStr = $items | select-object Name | sort-object { "$_".length } -descending | select-object -first 1
-    $len = $lnStr.name.length
-    $cols = If ($len) {($width+1)/($len+2)} Else {1};
-    $cols = [math]::floor($cols);
-    if(!$cols){ $cols=1;}
-
-    $color_fore = $Host.UI.RawUI.ForegroundColor
-
-    $compressed_list = @(".7z", ".gz", ".rar", ".tar", ".zip")
-    $executable_list = @(".exe", ".bat", ".cmd", ".py", ".pl", ".ps1",
-                         ".psm1", ".vbs", ".rb", ".reg", ".fsx")
-    $dll_pdb_list = @(".dll", ".pdb")
-    $text_files_list = @(".csv", ".lg", "markdown", ".rst", ".txt")
-    $configs_list = @(".cfg", ".config", ".conf", ".ini")
-
-    $color_table = @{}
-    foreach ($Extension in $compressed_list) {
-        $color_table[$Extension] = "Yellow"
-    }
-
-    foreach ($Extension in $executable_list) {
-        $color_table[$Extension] = "Blue"
-    }
-
-    foreach ($Extension in $text_files_list) {
-        $color_table[$Extension] = "Cyan"
-    }
-
-    foreach ($Extension in $dll_pdb_list) {
-        $color_table[$Extension] = "Darkgreen"
-    }
-
-    foreach ($Extension in $configs_list) {
-        $color_table[$Extension] = "DarkYellow"
-    }
-
-    $i = 0
-    $pad = [math]::ceiling(($width+2) / $cols) - 3
-    $nnl = $false
-
-    $items |
-    %{
-        if ($_.GetType().Name -eq 'DirectoryInfo') {
-            $c = 'Green'
-            $length = ""
-        } else {
-            $c = $color_table[$_.Extension]
-
-            if ($c -eq $none) {
-                $c = $color_fore
-            }
-
-            $length = $_.length
-        }
-
-        # get the directory name
-        if ($_.GetType().Name -eq "FileInfo") {
-            $DirectoryName = $_.DirectoryName
-        } elseif ($_.GetType().Name -eq "DirectoryInfo") {
-            $DirectoryName = $_.Parent.FullName
-        }
-        
-        if ($ifwide) {  # Wide (ls)
-            if ($LastDirectoryName -ne $DirectoryName) {  # change this to `$LastDirectoryName -ne $DirectoryName` to show DirectoryName
-                if($i -ne 0 -AND $host.ui.rawui.CursorPosition.X -ne 0){ # conditionally add an empty line
-                    write-host ""
-                }
-                Write-Host -Fore $color_fore ("`n   Directory: $DirectoryName`n")
-            }
-
-            $nnl = ++$i % $cols -ne 0
-
-            # truncate the item name
-            $towrite = $_.Name
-            if ($towrite.length -gt $pad) {
-                $towrite = $towrite.Substring(0, $pad - 3) + "..."
-            }
-
-            Write-Host ("{0,-$pad}" -f $towrite) -Fore $c -NoNewLine:$nnl
-            if($nnl){
-                write-host "  " -NoNewLine
-            }
-        } else {
-            If ($LastDirectoryName -ne $DirectoryName) {  # first item - print out the header
-                Write-Host "`n    Directory: $DirectoryName`n"
-                Write-Host "Mode                LastWriteTime     Length Name"
-                Write-Host "----                -------------     ------ ----"
-            }
-            $Host.UI.RawUI.ForegroundColor = $c
-
-            Write-Host ("{0,-7} {1,25} {2,10} {3}" -f $_.mode,
-                        ([String]::Format("{0,10}  {1,8}",
-                                          $_.LastWriteTime.ToString("d"),
-                                          $_.LastWriteTime.ToString("t"))),
-                        $length, $_.name)
-           
-            $Host.UI.RawUI.ForegroundColor = $color_fore
-
-            ++$i  # increase the counter
-        }
-        $LastDirectoryName = $DirectoryName
-    }
-
-    if ($nnl) {  # conditionally add an empty line
-        Write-Host ""
-    }
-}
-
-function Get-ChildItem-Format-Wide {
-    $New_Args = @($true)
-    $New_Args += "$Args"
-    Invoke-Expression "Get-ChildItem-Color $New_Args"
-}
 
 #function prompt
 #{
 #    "PS" + " [$(Get-Date -format T)] " + $(get-location)+">"
 #}
 
-Set-Alias ls Get-ChildItem-Color -option AllScope -Force
-Set-Alias dir Get-ChildItem-Color -option AllScope -Force
+$GitPromptSettings.DefaultPromptPrefix = '[$(Get-Date -format T)] '
+
+Import-Module posh-git
